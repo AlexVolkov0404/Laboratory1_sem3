f2c6a3150def5e2ad389e37ccc13636ad37472d0
Sasha <Sasha@ACER>
2022-10-04 09:13:37 +0300

    new functions to work with lists

--- Laboratory1/Laboratory1.cpp
+++ Laboratory1/Laboratory1.cpp
@@ -59,6 +59,212 @@ struct Node {
     Node* next;
     Node* prev;
 };
+struct List {
+    Node* head = nullptr;
+    Node* tail = nullptr;
+    int push() {
+        Book book;
+        FILE* b;
+        if ((b = fopen("books.txt", "r+b")) == NULL) {
+            perror("Error occured while opening file");
+            return 1;
+        }
+        while (fread(&book, sizeof(class Book), 1, b) == 1) {
+            Node* ptr = new Node;
+            ptr->next = NULL;
+            ptr->l_amount_of_pages = book.amount_of_pages;
+            strcpy(ptr->l_annottation, book.annottation);
+            strcpy(ptr->l_author, book.author);
+            ptr->l_date = book.date;
+            strcpy(ptr->l_name, book.name);
+            if (head != NULL) {
+                ptr->prev = tail;
+                tail->next = ptr;
+                tail = ptr;
+            }
+            else {
+                ptr->prev = NULL;
+                head = tail = ptr;
+            }
+        }
+        fclose(b);
+        return 0;
+    }
+    int push_last() {
+        /*Book book;*/
+        FILE* b;
+        if ((b = fopen("books.txt", "r+b")) == NULL) {
+            perror("Error occured while opening file");
+            return 1;
+        }
+        fseek(b, size_l * sizeof(class Book), 0);
+        fread(&book, sizeof(class Book), 1, b);
+        Node* ptr = new Node;
+        ptr->next = NULL;
+        ptr->l_amount_of_pages = book.amount_of_pages;
+        strcpy(ptr->l_annottation, book.annottation);
+        strcpy(ptr->l_author, book.author);
+        ptr->l_date = book.date;
+        strcpy(ptr->l_name, book.name);
+        if (head != NULL) {
+            ptr->prev = tail;
+            tail->next = ptr;
+            tail = ptr;
+        }
+        else {
+            ptr->prev = NULL;
+            head = tail = ptr;
+        }
+
+        fclose(b);
+        return 0;
+    }
+    int showl() {
+        Node* ptr = head;
+        while (ptr != NULL) {
+            cout << ptr->l_name << " " << ptr->l_author << " " << ptr->l_date << " " << ptr->l_amount_of_pages << " " << ptr->l_annottation << endl;
+            ptr = ptr->next;
+        }
+        return 0;
+    }
+    int search_d(string s) {
+
+        Node* ptr = head;
+
+        while (ptr != NULL) {
+
+            if (ptr->l_name == s) {
+                return ptr->l_date;
+
+            }
+
+            ptr = ptr->next;
+        }
+
+
+    }
+    int search_p(string s) {
+
+        Node* ptr = head;
+
+        while (ptr != NULL) {
+
+            if (ptr->l_name == s) {
+                return ptr->l_amount_of_pages;
+
+            }
+
+            ptr = ptr->next;
+        }
+
+
+    }
+    string search_a(string s) {
+
+        Node* ptr = head;
+
+        while (ptr != NULL) {
+
+            if (ptr->l_name == s) {
+                return ptr->l_author;
+
+            }
+
+            ptr = ptr->next;
+        }
+
+
+    }
+    int sort_big_amount_of_pages_buble_sort() {
+        /* Node* ptr = head;*/
+        int temp_p; // временная переменная для обмена элементов местами
+        int temp_d;
+        char temp_n[256];
+        char temp_a[256];
+        char temp_an[256];
+        // Сортировка массива пузырьком
+        for (int i = 0; i < size_l - 1; i++) {
+            Node* ptr = head;
+            for (int j = 0; j < size_l - i - 1; j++) {
+
+                if (ptr->l_amount_of_pages > ptr->next->l_amount_of_pages) {
+                    // меняем элементы местами
+                    temp_p = ptr->l_amount_of_pages;
+                    ptr->l_amount_of_pages = ptr->next->l_amount_of_pages;
+                    ptr->next->l_amount_of_pages = temp_p;
+
+                    temp_d = ptr->l_date;
+                    ptr->l_date = ptr->next->l_date;
+                    ptr->next->l_date = temp_d;
+
+                    strcpy(temp_n, ptr->l_name);
+                    strcpy(ptr->l_name, ptr->next->l_name);
+                    strcpy(ptr->next->l_name, temp_n);
+
+                    strcpy(temp_a, ptr->l_author);
+                    strcpy(ptr->l_author, ptr->next->l_author);
+                    strcpy(ptr->next->l_author, temp_a);
+
+                    strcpy(temp_an, ptr->l_annottation);
+                    strcpy(ptr->l_annottation, ptr->next->l_annottation);
+                    strcpy(ptr->next->l_annottation, temp_an);
+                }
+                ptr = ptr->next;
+            }
+        }
+
+        return 0;
+    }
+    int sort_big_name_clist_sort() {
+        Node* ptr = head;
+        while (ptr != NULL) {
+            book_names.push_back(ptr->l_name);
+            ptr = ptr->next;
+        }
+
+        return 0;
+    }
+    int sort_big_date_quick_sort() {
+        /* Node* ptr = head;*/
+        int temp_p; // временная переменная для обмена элементов местами
+        int temp_d;
+        char temp_n[256];
+        char temp_a[256];
+        char temp_an[256];
+        // Сортировка массива пузырьком
+        for (int i = 0; i < size_l - 1; i++) {
+            Node* ptr = head;
+            for (int j = 0; j < size_l - i - 1; j++) {
+
+                if (ptr->l_date > ptr->next->l_date) {
+                    // меняем элементы местами
+                    temp_p = ptr->l_amount_of_pages;
+                    ptr->l_amount_of_pages = ptr->next->l_amount_of_pages;
+                    ptr->next->l_amount_of_pages = temp_p;
+
+                    temp_d = ptr->l_date;
+                    ptr->l_date = ptr->next->l_date;
+                    ptr->next->l_date = temp_d;
+
+                    strcpy(temp_n, ptr->l_name);
+                    strcpy(ptr->l_name, ptr->next->l_name);
+                    strcpy(ptr->next->l_name, temp_n);
+
+                    strcpy(temp_a, ptr->l_author);
+                    strcpy(ptr->l_author, ptr->next->l_author);
+                    strcpy(ptr->next->l_author, temp_a);
+
+                    strcpy(temp_an, ptr->l_annottation);
+                    strcpy(ptr->l_annottation, ptr->next->l_annottation);
+                    strcpy(ptr->next->l_annottation, temp_an);
+                }
+                ptr = ptr->next;
+            }
+        }
+
+        return 0;
+    }
+};
 int main()
 {
    
